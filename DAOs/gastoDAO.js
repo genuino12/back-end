6893e9d6dbb9c94ba340d41d0a7410159de465f9
genuino12 <lucasgenuino567@gmail.com>
2024-11-25 23:54:26 -0300

    atualizar

--- DAOs/gastoDAO.js
+++ DAOs/gastoDAO.js
@@ -1,18 +1,30 @@
-const db = require('../config/db.js');
+const db = require('../config/db.js');  
 class gastoDAO {
     async inserir(gasto) {
         const query = `INSERT INTO despesas (tipo_despesa_id, valor, data_vencimento, responsavel_nome, observacao)
                        VALUES (?, ?, ?, ?, ?)`;
-        
-        const [result] = await db.execute(query, [
-            gasto.tipo_despesa, 
-            gasto.valor, 
-            gasto.data_vencimento, 
-            gasto.responsavel_nome, 
-            gasto.observacao
-        ]);
 
-        return result.insertId;  
+       
+        const tipo_despesa = gasto.tipo_despesa || null;
+        const valor = gasto.valor || null;
+        const data_vencimento = gasto.data_vencimento || null;
+        const responsavel_nome = gasto.responsavel_nome || null;
+        const observacao = gasto.observacao || null;
+
+        try {
+            const [result] = await db.execute(query, [
+                tipo_despesa, 
+                valor, 
+                data_vencimento, 
+                responsavel_nome, 
+                observacao
+            ]);
+
+            return result.insertId; 
+        } catch (error) {
+            console.error('Erro ao inserir gasto:', error);
+            throw error;  
+        }
     }
 }
 
